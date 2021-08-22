2e8b30fc1b1d26ea742686a493d3b57d29c45ff6
1cy50ng3b0b <66748817+CollinEdward@users.noreply.github.com>
2021-08-22 09:07:02 +0000

    Clean Up

--- CountToNumber.py
+++ CountToNumber.py
@@ -1,20 +1,17 @@
-def CountToNumber():
-    x = int(input("please enter a number: "))
-    y = 0
-    
+def script()
+    def CountToNumber():
+        x = int(input("please enter a number: "))
+        y = 0
+        while y < x:
+            print(y)
+            y += 1
+            if y == x:
+                print(x)        
+                if x <= 100 * 10000:
+                    break 
+                else:
+                    pass
 
-    while y < x:
-        print(y)
-        y += 1
-    
-
-        if y == x:
-            print(x)
-            
-
-            if x <= 100 * 10000:
-                break 
-            else:
-                pass
-
-CountToNumber()
+    CountToNumber()
+if __name__ == "__main__":
+    script()
